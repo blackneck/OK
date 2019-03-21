f235d8e8a844c02d6045205f72b68a81734b960b
Alexander <demoman361@gmail.com>
2019-03-21 20:29:17 +0300

    add rn-fetch blob

--- android/app/src/main/java/com/OK/MainApplication.java
+++ android/app/src/main/java/com/OK/MainApplication.java
@@ -3,6 +3,7 @@ package com.OK;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.wheelpicker.WheelPickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new VectorIconsPackage(),
             new WheelPickerPackage(),
             new RNFirebasePackage(),
